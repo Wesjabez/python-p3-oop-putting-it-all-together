86a367f8ebb87ce45c40c35bb43c6763bb72cfb8
Wesley <wesochienga@gmail.com>
2023-08-25 20:31:08 +0300

    first

--- lib/shoe.py
+++ lib/shoe.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 class Shoe:
+    def __init__(self, brand, size):
+        self.brand = brand
+        self.size = size
+        self.condition = "New"  # Initialize condition attribute
+
+    def cobble(self):
+        self.condition = "New"
+        print("Your shoe is as good as new!")
+
+if __name__ == "__main__":
     pass
